c32f1bb1c0479b42c33cc4aee152ccb577bfb964
jonber695 <64254502+jonber695@users.noreply.github.com>
2021-01-02 11:34:01 +0100

    cleaned up some code and changed the PK to serial instead of int

--- log-phys model.sql
+++ log-phys model.sql
@@ -1,5 +1,5 @@
 CREATE TABLE admin_staff (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  employee_id VARCHAR(50) NOT NULL,
  first_name VARCHAR(50),
  last_name VARCHAR(50),
@@ -12,7 +12,7 @@ ALTER TABLE admin_staff ADD CONSTRAINT PK_admin_staff PRIMARY KEY (id);
 
 
 CREATE TABLE instuctor (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  first_name VARCHAR(50),
  last_name VARCHAR(50),
  age VARCHAR(10),
@@ -42,7 +42,7 @@ ALTER TABLE level_of_lesson_price ADD CONSTRAINT PK_level_of_lesson_price PRIMAR
 
 
 CREATE TABLE parent (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  street_name VARCHAR(50),
  street_number VARCHAR(10),
  zip VARCHAR(5)
@@ -52,14 +52,14 @@ ALTER TABLE parent ADD CONSTRAINT PK_parent PRIMARY KEY (id);
 
 
 CREATE TABLE schedule (
- id INT NOT NULL
+ id SERIAL NOT NULL
 );
 
 ALTER TABLE schedule ADD CONSTRAINT PK_schedule PRIMARY KEY (id);
 
 
 CREATE TABLE sibling_dicount (
- parent_id INT NOT NULL,
+ parent_id SERIAL NOT NULL,
  amount NUMERIC(32)
 );
 
@@ -75,7 +75,7 @@ ALTER TABLE special_day_addon_price ADD CONSTRAINT PK_special_day_addon_price PR
 
 
 CREATE TABLE student (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  first_name VARCHAR(50) NOT NULL,
  last_name VARCHAR(50) NOT NULL,
  age VARCHAR(10) NOT NULL,
@@ -86,14 +86,14 @@ CREATE TABLE student (
  number_of_lessons INT,
  current_skill VARCHAR(50) NOT NULL,
  number_of_instruments_rented INT,
- parent_id INT NOT NULL
+ parent_id SERIAL NOT NULL
 );
 
 ALTER TABLE student ADD CONSTRAINT PK_student PRIMARY KEY (id);
 
 
 CREATE TABLE student_payment (
- student_id INT NOT NULL,
+ student_id SERIAL NOT NULL,
  amount NUMERIC(32)
 );
 
@@ -101,29 +101,29 @@ ALTER TABLE student_payment ADD CONSTRAINT PK_student_payment PRIMARY KEY (stude
 
 
 CREATE TABLE application (
- student_id INT NOT NULL,
+ student_id SERIAL NOT NULL,
  first_name VARCHAR(50) NOT NULL,
  last_name VARCHAR(50) NOT NULL,
  age VARCHAR(10) NOT NULL,
  personal_number VARCHAR(12) NOT NULL,
  instrument VARCHAR(50) NOT NULL,
- admin_staff_id INT
+ admin_staff_id SERIAL
 );
 
 ALTER TABLE application ADD CONSTRAINT PK_application PRIMARY KEY (student_id);
 
 
 CREATE TABLE appointment (
- schedule_id INT NOT NULL,
+ schedule_id SERIAL NOT NULL,
  time TIMESTAMP(10),
- date VARCHAR(10)
+ date DATE
 );
 
 ALTER TABLE appointment ADD CONSTRAINT PK_appointment PRIMARY KEY (schedule_id);
 
 
 CREATE TABLE audition (
- student_id INT NOT NULL,
+ student_id SERIAL NOT NULL,
  is_passed VARCHAR(5)
 );
 
@@ -141,18 +141,18 @@ ALTER TABLE email ADD CONSTRAINT PK_email PRIMARY KEY (email,student_id,parent_i
 
 
 CREATE TABLE individual_lesson (
- schedule_id INT NOT NULL,
+ schedule_id SERIAL NOT NULL,
  level VARCHAR(50),
  instrument VARCHAR(50),
- student_id INT NOT NULL,
- instructor_id INT NOT NULL
+ student_id SERIAL NOT NULL,
+ instructor_id SERIAL NOT NULL
 );
 
 ALTER TABLE individual_lesson ADD CONSTRAINT PK_individual_lesson PRIMARY KEY (schedule_id);
 
 
 CREATE TABLE instructor_payment (
- instructor_id INT NOT NULL,
+ instructor_id SERIAL NOT NULL,
  amount NUMERIC(32)
 );
 
@@ -160,7 +160,7 @@ ALTER TABLE instructor_payment ADD CONSTRAINT PK_instructor_payment PRIMARY KEY 
 
 
 CREATE TABLE number_of_children (
- parent_id INT NOT NULL,
+ parent_id SERIAL NOT NULL,
  number_of_children INT
 );
 
@@ -169,72 +169,72 @@ ALTER TABLE number_of_children ADD CONSTRAINT PK_number_of_children PRIMARY KEY 
 
 CREATE TABLE phone_number (
  phone_number VARCHAR(15) NOT NULL,
- parent_id INT NOT NULL,
- student_id INT NOT NULL,
- instructor_id INT NOT NULL
+ parent_id SERIAL NOT NULL,
+ student_id SERIAL NOT NULL,
+ instructor_id SERIAL NOT NULL
 );
 
 ALTER TABLE phone_number ADD CONSTRAINT PK_phone_number PRIMARY KEY (phone_number,parent_id,student_id,instructor_id);
 
 
 CREATE TABLE rental_agreement_for_instrument (
- student_id INT NOT NULL,
+ student_id SERIAL NOT NULL,
  length INT,
- start_date VARCHAR(15),
- end_date VARCHAR(15)
+ start_date DATE,
+ end_date DATE
 );
 
 ALTER TABLE rental_agreement_for_instrument ADD CONSTRAINT PK_rental_agreement_for_instrument PRIMARY KEY (student_id);
 
 
 CREATE TABLE schedualed_time_slot (
- schedule_id INT NOT NULL,
+ schedule_id SERIAL NOT NULL,
  time TIMESTAMP(10),
- date VARCHAR(10)
+ date DATE
 );
 
 ALTER TABLE schedualed_time_slot ADD CONSTRAINT PK_schedualed_time_slot PRIMARY KEY (schedule_id);
 
 
 CREATE TABLE ensemble (
- schedule_id INT NOT NULL,
+ schedule_id SERIAL NOT NULL,
  target_genre VARCHAR(50),
  min_number_of_students VARCHAR(10) NOT NULL,
  max_number_of_students VARCHAR(10) NOT NULL,
- instructor_id INT NOT NULL,
- student_id INT NOT NULL
+ instructor_id SERIAL NOT NULL,
+ student_id SERIAL NOT NULL
 );
 
 ALTER TABLE ensemble ADD CONSTRAINT PK_ensemble PRIMARY KEY (schedule_id);
 
 
 CREATE TABLE group_lesson (
- schedule_id INT NOT NULL,
+ schedule_id SERIAL NOT NULL,
  min_number_of_students VARCHAR(10) NOT NULL,
  max_number_of_students VARCHAR(10) NOT NULL,
  level VARCHAR(50),
  Instrument VARCHAR(50),
- instructor_id INT NOT NULL,
- student_id INT NOT NULL
+ instructor_id SERIAL NOT NULL,
+ student_id SERIAL NOT NULL
 );
 
 ALTER TABLE group_lesson ADD CONSTRAINT PK_group_lesson PRIMARY KEY (schedule_id);
 
 
 CREATE TABLE instrument (
- id INT NOT NULL,
+ id SERIAL NOT NULL,
  brand VARCHAR(50) NOT NULL,
  type VARCHAR(50) NOT NULL,
  quantity INT NOT NULL,
  price NUMERIC(32) NOT NULL,
- student_id INT
+ student_id SERIAL
 );
 
 ALTER TABLE instrument ADD CONSTRAINT PK_instrument PRIMARY KEY (id);
 
 
 CREATE TABLE price (
- schedule_id INT NOT NULL,
+ schedule_id SERIAL NOT NULL,
  lesson_type VARCHAR(50) NOT NULL,
  level_of_lesson VARCHAR(50) NOT NULL,
  special_day_addon VARCHAR(50) NOT NULL
